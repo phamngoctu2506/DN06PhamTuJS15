e2c67ac6d2c912d0424aef370200c7d1654218e5
Phamtu <phamngoctu2506@gmail.com>
2022-10-20 12:20:32 +0700

    update

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@
  *          + lấy giá trị từ form
  *          + diemChuan (lấy giá trị từ form)
  *          + diemKV => cần 3 khu vực để kiểm tra
- *                          !=> trả kết quả tongDiem
+ *                      
  *          + diemDT => cần 3 loại đối tượng để kiểm tra
- *                          !=> trả kết quả tongDiem
+ *                        
  *          + tongDiem => cần tổng diemTungMon + diemKhuVuc + diemDoiTuong
- *                           !=> trả kết quả ketQuaTinh
+ *                           
  *          + tạo hàm if else phân loại điều kiện
- *                           !=> trả kết quả 
+ *                      
  * 
  *  
  * 
@@ -29,60 +29,21 @@ function ketQuaTinh() {
     var Mon3 = Number(document.getElementById("number6").value);
     var diemKV = Number(document.getElementById("exampleFormControlSelect1").value);
     var diemDT = Number(document.getElementById("exampleFormControlSelect12").value);
-    console.log(diemChuan, Mon1, Mon2, Mon3, diemKV, diemDT);
-    var diem3Mon = Mon1 + Mon2 + Mon3;
-    // if (tongKetDiem >= diemChuan) {
-    //     console.log('đạt');
-    // } else if (tongKetDiem < diemChuan) {
-    //     console.log('rớt');
-    // } else if (Mon1 == 0 || Mon2 == 0 || Mon3 == 0) {
-    //     console.log('bạn bị liệt môn => rớt');
-    // } else {
 
-    // }
-    function diemDT() {
-        //kiểm tra: if-else if, checked(radio, checkbox) => true
-    
-        var diemDT = "";
-    
-        if (diemDT = 1) {
-            diemDoiTuong = 2.5;
-        } else if (diemDT = 2) {
-            diemDoiTuong = 1.5;
-        } else if (diemDT = 3) {
-            diemDoiTuong = 1;
-        } else if (diemDT = 0) {
-            diemDoiTuong = 0;
-        } else {
-        }
-    
-        return tongDiem;
-    
+    var diem3Mon = Mon1 + Mon2 + Mon3;
+
+    var tongDiem = diem3Mon + diemKV + diemDT;
+
+    console.log(tongDiem);
+    if (tongDiem >= diemChuan && Mon1 > 0 && Mon2 > 0 && Mon3 > 0) {
+        console.log('đạt');
+        document.getElementById('ketQuaTraVe0').innerHTML = 'tổng điểm là: ' + tongDiem + " Bạn Đã đậu, xin chúc mừng";
+    } else if (tongDiem < diemChuan || Mon1 > 0 || Mon2 > 0 || Mon3 > 0) {
+        console.log('không đạt');
+        document.getElementById('ketQuaTraVe0').innerHTML = 'tổng điểm là: ' + tongDiem + " rớt, xin chia buồn :))";
+    } else {
+
     }
-    function diemKV() {
-        //kiểm tra: if-else if, checked(radio, checkbox) => true
-    
-        var diemKV = "";
-    
-        if (diemDT = 1) {
-            diemKhuVuc = 2;
-        } else if (diemDT = 2) {
-            diemKhuVuc = 1;
-        } else if (diemDT = 3) {
-            diemKhuVuc = 0.5;
-        } else if (diemDT = 0) {
-            diemKhuVuc = 0;
-        } else {
-        }
-        return tongDiem;
-    }
-    function tongDiem() {
-    
-        tongKetDiem = diem3Mon + diemDoiTuong + diemKhuVuc;
-        console.log(tongKetDiem, diemTungMon)
-        return ketQuaTinh;
-    }
-    
 }
 
 
